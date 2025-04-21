62f322bb44e91bba2f46a5f1c95fce9d874ef7a3
filthywill <95443078+filthywill@users.noreply.github.com>
2025-04-21 16:35:24 -0400

    SM_dis

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import './lib/fontawesome';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
